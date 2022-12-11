56ab173cab31dcfe93c0f9e151c0504b49333464
Roman <alvur94@mail.ru>
2022-12-11 23:32:19 +0300

    добавлен код решения задачи 2 в проект task2

--- HW1/task2/Program.cs
+++ HW1/task2/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
+if(firstNumber < secondNumber)
+{
+    Console.Write("max = {0}, min = 2{1}", secondNumber, firstNumber);
+}
+else
+{
+    Console.Write("max = {0}, min = {1}", firstNumber, secondNumber);
+}
