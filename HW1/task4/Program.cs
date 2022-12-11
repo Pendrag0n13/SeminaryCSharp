dddc1a6f2f5bb19b92980f749856375e5d3f4460
Roman <alvur94@mail.ru>
2022-12-11 23:37:44 +0300

    добавлен код решения задачи 4 в проект task4

--- HW1/task4/Program.cs
+++ HW1/task4/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число a: ");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b : ");
+int NumberB = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число c: ");
+int NumberC = Convert.ToInt32(Console.ReadLine());
+int max = Math.Max(Math.Max(NumberA, NumberB), NumberC);
+Console.Write("Максимальное число = {0}", max);
