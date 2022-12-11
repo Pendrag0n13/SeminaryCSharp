b00064b5f1cb5b7bd0c9e6b787bbb983510f9d2d
Roman <alvur94@mail.ru>
2022-12-11 23:44:17 +0300

    добавлен код решения задачи 8 в проект task8

--- HW1/task8/Program.cs
+++ HW1/task8/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число N: ");
+int NumberN = Convert.ToInt32(Console.ReadLine());
+Console.Write("Чётный числа от 1 до {0}:", NumberN);
+for(int i = 2; i <= NumberN; i += 2)
+{
+    Console.Write(" {0}", i);
+} 
