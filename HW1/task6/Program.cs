e9ac95ff44352f6019dc228782f5ff058b69a4f5
Roman <alvur94@mail.ru>
2022-12-11 23:40:13 +0300

    добавлен код решения задачи 6 в проект task6

--- HW1/task6/Program.cs
+++ HW1/task6/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Write("Введите число: ");
+int NumberA = Convert.ToInt32(Console.ReadLine());
+if(NumberA % 2 == 0)
+{
+    Console.Write("Число чётное");
+}
+else
+{
+    Console.Write("Число нечётное");
+}  
