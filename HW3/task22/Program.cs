c26d417157c0556dd526bdc42823283fdb6ae222
Roman <alvur94@mail.ru>
2022-12-17 23:16:46 +0300

    добавлен массив для хранения значений, чтобы выполнить доп задание

--- HW3/task22/Program.cs
+++ HW3/task22/Program.cs
@@ -6,16 +6,18 @@
 
 Доп. задание
 Написать метод для разворота массива, не используя память под дополнительный массив
+
 */
 String s = String.Empty;
 do
-{
+{    
     Console.Write("Введите число N: ");
     int N = Convert.ToInt32(Console.ReadLine());
+    double[] res = new double[N];
     for(int i = 1; i <= N; i++)
     {
-        var res = Math.Pow(i, 2);
-        Console.Write($"{res:f0} ");
+        res[i-1] = Math.Pow(i, 2);
+        Console.Write($"{res[i-1]:f0} ");
     }
     Console.WriteLine();
     Console.WriteLine("Завершить программу y/n ?");
