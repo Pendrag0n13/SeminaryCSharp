e449cf1168446124fc55af57a32ffdcd82836f8f
Roman <alvur94@mail.ru>
2022-12-17 23:57:13 +0300

    добавлено преобразование в массив и проверка на полиндром

--- HW3/task19/Program.cs
+++ HW3/task19/Program.cs
@@ -9,13 +9,26 @@
 String s = String.Empty;
 do
 {
-    Console.Write("Введите номер четверти: ");
+    Console.Write("Введите пятизначное число: ");
     String inputStr = String.Empty;
     inputStr = Console.ReadLine();
     int number = Convert.ToInt32(inputStr);
+    Console.WriteLine();
+    int[] numberArr = new int[5];
     if((number > 9999) && (number < 100000))
     {
-        
+        for(int i = 0; i < 5; i++)
+        {
+            numberArr[i] = Convert.ToInt32(inputStr[i]);
+        }
+        if (numberArr[0] == numberArr[4] && numberArr[1] == numberArr[3])
+        {
+            Console.WriteLine($"{number} - Палиндром");
+        }
+        else
+        {
+            Console.WriteLine($"{number} - НЕ палиндром");
+        }
     }
     else
     {
