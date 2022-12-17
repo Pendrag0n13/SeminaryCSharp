481cf6c1aa35fa7ba1e00edee083800e5fc485d6
Roman <alvur94@mail.ru>
2022-12-17 23:37:26 +0300

    добавлен решения для доп задание

--- HW3/task22/Program.cs
+++ HW3/task22/Program.cs
@@ -13,11 +13,31 @@ do
 {    
     Console.Write("Введите число N: ");
     int N = Convert.ToInt32(Console.ReadLine());
-    double[] res = new double[N];
-    for(int i = 1; i <= N; i++)
+    if (N > 0)
     {
-        res[i-1] = Math.Pow(i, 2);
-        Console.Write($"{res[i-1]:f0} ");
+        double[] res = new double[N];
+        for(int i = 1; i <= N; i++)
+        {
+            res[i-1] = Math.Pow(i, 2);
+            Console.Write($"{res[i-1]:f0} ");
+        }
+        Console.WriteLine();
+        Console.WriteLine("Разворот массива:");
+        double tmp = 0;
+        for(int i = 0; i < N/2; i++)
+        {
+            tmp = res[i];
+            res[i] = res[N-1-i];
+            res[N-1-i] = tmp;
+        }
+        for(int i = 0; i < N; i++)
+        {
+            Console.Write($"{res[i]:f0} ");
+        }
+    }
+    else
+    {
+        Console.WriteLine("N < 0 !");
     }
     Console.WriteLine();
     Console.WriteLine("Завершить программу y/n ?");
