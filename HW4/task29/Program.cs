17d1a7ae99fe38b43106f8c2c6e123d004efc899
Roman <alvur94@mail.ru>
2022-12-19 00:45:00 +0300

    добавлена функция void main() и её вызов, а также исправлены функции CreateArray, FillingArray и функция GetNumber() переименована в GetNumber2()

--- HW4/task29/Program.cs
+++ HW4/task29/Program.cs
@@ -7,22 +7,40 @@ int GetNumber(string message)
     return Convert.ToInt32(Console.ReadLine());
 }
 
-int GetNumber()
+int GetNumber2()
 {
     return Convert.ToInt32(Console.ReadLine());
 }
 
 int[] CreateArray(int N)
 {
-    int[] mass = new int[N];    
+    int[] mass = new int[N];
+    mass = FillingArray (mass);
+    return mass; 
 }
 
-int[] FillingArray(int mass[])
+int[] FillingArray(int[] mass)
 {
     for(int i = 0; i < mass.Length; i++)
     {
-        mass[i] = GetNumber();
+        mass[i] = GetNumber2();
     }
 
     return mass;
 }
+
+void main()
+{
+    String s = String.Empty;
+    do
+    { 
+        int number = GetNumber("Введите размер мссива: ");
+        Console.WriteLine("Заполните массив:");
+        Console.WriteLine($"[{string.Join(", ", CreateArray(number))}]");
+        Console.WriteLine("Завершить программу y/n ?");
+        s = Console.ReadLine();
+    }while(s != "y");
+
+}
+
+main();
