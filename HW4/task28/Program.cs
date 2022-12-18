b708faacc870dd5b4472df09ed369bdb5548a37b
Roman <alvur94@mail.ru>
2022-12-18 23:20:27 +0300

    добавлена функция int GetNumber(string message)

--- HW4/task28/Program.cs
+++ HW4/task28/Program.cs
@@ -1,3 +1,9 @@
 ﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
 // 4 -> 24 
 // 5 -> 120
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
