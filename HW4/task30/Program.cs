8c41358b016abdf13f8b13870eada40f6d364bd8
Roman <alvur94@mail.ru>
2022-12-18 23:45:27 +0300

    добавлена функция int[] CreateArray0and1(int N)

--- HW4/task30/Program.cs
+++ HW4/task30/Program.cs
@@ -2,3 +2,13 @@
 // заполненный нулями и единицами в случайном порядке.
 // [1,0,1,1,0,1,0,0]
 
+int[] CreateArray0and1(int N)
+{
+    var rand = new Random();
+    int[] mass = new int [N];
+    for(int i = 0; i < N; i++)
+    {
+        mass[i] = rand.Next(0, 2);
+    }
+    return mass;
+}
