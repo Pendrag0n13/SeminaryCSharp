de609fad7329e9dbbed54459ced1458e9a4cd1a4
Roman <alvur94@mail.ru>
2022-12-19 00:30:15 +0300

    добавлена функция int GetNumber(string message)

--- HW4/task29/Program.cs
+++ HW4/task29/Program.cs
@@ -1,2 +1,8 @@
 ﻿// Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. 
 // Данные вводятся с консоли пользователем
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
