c80e949468b3e9611bec5909bac29b89d30f1ad5
Roman <alvur94@mail.ru>
2022-12-20 00:13:06 +0300

    добавлена функция int[] CreateArray(int N, int leftRange, int rightRange)

--- HW5/task32/Program.cs
+++ HW5/task32/Program.cs
@@ -1,3 +1,15 @@
 ﻿// Задача 32: Напишите программу замена элементов массива: положительные элементы замените на
 // соответствующие отрицательные, и наоборот.
 // [-4, -8, 8, 2] -> [4, 8, -8, -2]
+
+int[] CreateArray(int N, int leftRange, int rightRange)
+{
+    var rand = new Random();
+    int[] mass = new int [N];
+    for(int i = 0; i < mass.Length; i++)
+    {
+        mass[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return mass;
+}
+
