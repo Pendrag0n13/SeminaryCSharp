26494ade418067c3b260fc9883f19b89f110d3e2
Roman <alvur94@mail.ru>
2022-12-20 01:38:39 +0300

    добавлена функция int[] CreateArray(int N, int leftRange, int rightRange)

--- HW5/task38/Program.cs
+++ HW5/task38/Program.cs
@@ -1,3 +1,13 @@
 ﻿// Задача 38: Задайте массив натуральных чисел. 
 // Найдите разницу между максимальным и минимальным элементов массива.
 // [3 7 22 2 78] -> 76
+int[] CreateArray(int N, int leftRange, int rightRange)
+{
+    var rand = new Random();
+    int[] mass = new int [N];
+    for(int i = 0; i < mass.Length; i++)
+    {
+        mass[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return mass;
+}
