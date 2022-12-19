7a337db0fa3223911b980483286017e44e04329e
Roman <alvur94@mail.ru>
2022-12-19 23:47:23 +0300

    в функции CreateArray исправлены ошибки

--- HW5/task31/Program.cs
+++ HW5/task31/Program.cs
@@ -7,9 +7,11 @@ int[] CreateArray(int N, int leftRange, int rightRange)
 {
     var rand = new Random();
     int[] mass = new int [N];
-    for(int i = 0; i < N; i++)
+    for(int i = 0; i < mass.Length; i++)
     {
-        mass[i] = rand.Next(leftRange, rightRange);
+        mass[i] = rand.Next(leftRange, rightRange + 1);
     }
     return mass;
 }
+
+
