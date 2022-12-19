602276bdc2ddf191df1329af3aa0d5ce987b2700
Roman <alvur94@mail.ru>
2022-12-20 01:03:49 +0300

    добавлена функция int[] CreateArray(int N, int leftRange, int rightRange)

--- HW5/task37/Program.cs
+++ HW5/task37/Program.cs
@@ -3,3 +3,14 @@
 // Результат запишите в новом массиве.
 // [1 2 3 4 5] -> 5 8 3
 // [6 7 3 6] -> 36 21
+
+int[] CreateArray(int N, int leftRange, int rightRange)
+{
+    var rand = new Random();
+    int[] mass = new int [N];
+    for(int i = 0; i < mass.Length; i++)
+    {
+        mass[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return mass;
+}
