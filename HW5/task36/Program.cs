d2b5a203b02d0632eb005783fe05c052120788cb
Roman <alvur94@mail.ru>
2022-12-20 01:29:08 +0300

    добавлена функция int[] CreateArray(int N, int leftRange, int rightRange)

--- HW5/task36/Program.cs
+++ HW5/task36/Program.cs
@@ -2,3 +2,14 @@
 // Найдите сумму элементов, стоящих на нечётных позициях.
 // [3, 7, 23, 12] -> 19
 // [-4, -6, 89, 6] -> 0
+
+int[] CreateArray(int N, int leftRange, int rightRange)
+{
+    var rand = new Random();
+    int[] mass = new int [N];
+    for(int i = 0; i < mass.Length; i++)
+    {
+        mass[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return mass;
+}
