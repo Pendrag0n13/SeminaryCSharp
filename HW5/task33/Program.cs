e2e0cb46ae372b3374fe3d43aca7abd4cf30b9eb
Roman <alvur94@mail.ru>
2022-12-20 00:27:36 +0300

    добавлена функция int[] CreateArray(int N, int leftRange, int rightRange)

--- HW5/task33/Program.cs
+++ HW5/task33/Program.cs
@@ -1,3 +1,13 @@
 ﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
 // 4; массив [6, 7, 19, 345, 3] -> нет
 // 3; массив [6, 7, 19, 345, 3] -> да
+int[] CreateArray(int N, int leftRange, int rightRange)
+{
+    var rand = new Random();
+    int[] mass = new int [N];
+    for(int i = 0; i < mass.Length; i++)
+    {
+        mass[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return mass;
+}
