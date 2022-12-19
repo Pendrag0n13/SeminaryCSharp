395a08e1fdd430b4dc3e2531e6079d2c0923146f
Roman <alvur94@mail.ru>
2022-12-20 01:21:39 +0300

    добавлена функция int[] CreateArray(int N, int leftRange, int rightRange)

--- HW5/task34/Program.cs
+++ HW5/task34/Program.cs
@@ -2,3 +2,13 @@
 // Напишите программу, которая покажет количество чётных чисел в массиве.
 // [345, 897, 568, 234] -> 2
 
+int[] CreateArray(int N, int leftRange, int rightRange)
+{
+    var rand = new Random();
+    int[] mass = new int [N];
+    for(int i = 0; i < mass.Length; i++)
+    {
+        mass[i] = rand.Next(leftRange, rightRange + 1);
+    }
+    return mass;
+}
