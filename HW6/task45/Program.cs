81748a17110f3950e1598fa7aaf3b0ea2383254a
Roman <alvur94@mail.ru>
2022-12-24 22:45:48 +0300

    добавлена функция int[] CopyArray(int[] mass) и её вызов в main()

--- HW6/task45/Program.cs
+++ HW6/task45/Program.cs
@@ -1,32 +1,54 @@
 ﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
 
-const int SIZE = 5;
+const int SIZE = 10;
 const int LEFTRANGE = 0;
-const int RIGHTRANGE = 100; 
+const int RIGHTRANGE = 100;
 
 int[] CreateArray(int N, int leftRange, int rightRange)
 {
     var rand = new Random();
-    int[] mass = new int [N];
-    for(int i = 0; i < mass.Length; i++)
+    int[] mass = new int[N];
+    for (int i = 0; i < mass.Length; i++)
     {
         mass[i] = rand.Next(leftRange, rightRange + 1);
     }
     return mass;
 }
 
+int[] CopyArray(int[] mass)
+{
+    int[] newMass = new int [mass.Length];
+    for(int i = 0; i < mass.Length; i++)
+    {
+        newMass[i] = mass[i];
+    }
+    return newMass;
+}
+
 void main()
 {
     String s = String.Empty;
     do
-    { 
-        int[] arr = CreateArray(SIZE, LEFTRANGE, rightRange);
-        
+    {
+        int[] arr1 = CreateArray(SIZE, LEFTRANGE, RIGHTRANGE);
+        arr1[0] = -1000;
+        Console.WriteLine($"[{string.Join(",", arr1)}]");
+
+        int[] arr2 = CopyArray(arr1);
+        arr1[0] = -5000;
+        Console.WriteLine($"[{string.Join(",", arr2)}]");
+
+        int[] arr3 = new int [arr1.Length];
+        for(int i = 0; i < arr3.Length; i++)
+        {
+            arr3[i] = arr1[i];
+        }
+        arr1[0] = -8000;
+        Console.WriteLine($"[{string.Join(",", arr3)}]");
         Console.WriteLine();
         Console.WriteLine("Завершить программу y/n ?");
         s = Console.ReadLine();
-    }while(s != "y");
-
+    } while (s != "y");
 }
 
 main();
