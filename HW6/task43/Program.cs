f4ebd96e73b9c7240444f39e0403ea05db7387be
Roman <alvur94@mail.ru>
2022-12-24 23:10:28 +0300

    добавлена функция double GetX(double k1, double b1, double k2, double b2)

--- HW6/task43/Program.cs
+++ HW6/task43/Program.cs
@@ -2,7 +2,13 @@
 // заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
-int b1;
-int b2;
-int k1;
-int k2;
+double b1;
+double b2;
+double k1;
+double k2;
+
+
+double GetX(double k1, double b1, double k2, double b2)
+{
+    return ((b2-b1)/(k1-k2));
+}
