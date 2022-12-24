1df209873e3de97d8f6470104313156d86d0c250
Roman <alvur94@mail.ru>
2022-12-24 23:00:10 +0300

    добавлена функция void main() и её вызов

--- HW6/task41/Program.cs
+++ HW6/task41/Program.cs
@@ -1,3 +1,21 @@
 ﻿// Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь. Ввод чисел останавливается при помощи ввода стоп-слова "stop"
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 4
+void main()
+{
+    int n;
+    int count = 0;
+    string str = Console.ReadLine();
+    while (str != "stop")
+    {
+        n = Convert.ToInt32(str);
+        if (n > 0)
+        {
+            count++;
+        }
+        str = Console.ReadLine();
+    }
+    Console.WriteLine($"[{count}]");
+}
+
+main();
