59dcf520c557279a48f1158859ee85a6a8497a20
Roman <alvur94@mail.ru>
2023-01-02 14:29:48 +0300

    добавлена функция double[,] CreateArrayMxN(int rowCount, int colCount, int leftRange = 0, int rightRange = 9)

--- HW7/task47/Program.cs
+++ HW7/task47/Program.cs
@@ -4,3 +4,17 @@
 // 0,5 7 -2 -0,2 ​
 // 1 -3,3 8 -9,9​
 // 8 7,8 -7,1 9
+
+double[,] CreateArrayMxN(int rowCount, int colCount, int leftRange = 0, int rightRange = 9)
+{
+    Random rand = new Random();
+    double[,] mass = new double[rowCount, colCount];
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            mass[i, j] = rand.NextDouble() * rightRange;
+        }
+    }
+    return mass;
+}
