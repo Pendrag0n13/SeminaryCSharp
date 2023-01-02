ab891290129d394d9c52cab99d7609d268be98ba
Roman <alvur94@mail.ru>
2023-01-02 12:30:22 +0300

    добавлена функция int[,] CreateArrayMxN(int M, int N, int leftRange, int rightRange)

--- HW7/task46/Program.cs
+++ HW7/task46/Program.cs
@@ -7,4 +7,18 @@
 const int SIZE_N = 5;
 const int SIZE_M = 5;
 const int LEFTRANGE = -99;
-const int RIGHTRANGE = 100;
+const int RIGHTRANGE = 99;
+
+int[,] CreateArrayMxN(int M, int N, int leftRange, int rightRange)
+{
+    var rand = new Random();
+    int[,] mass = new int [M, N];
+    for(int i = 0; i < M; i++)
+    {
+        for(int j = 0; j < N; j++)
+        {
+            mass[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return mass;
+}
