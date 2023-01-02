f0250791cb771eb17f4000b10d17d650e07abdab
Roman <alvur94@mail.ru>
2023-01-02 15:13:16 +0300

    добавлена функция int[,] CreateArrayMxN(int rowCount, int colCount, int leftRange = 0, int rightRange = 9)

--- HW7/task52/Program.cs
+++ HW7/task52/Program.cs
@@ -6,3 +6,17 @@
 // 5 9 2 3
 // 8 4 2 4
 // Среднее арифметическое каждого столбца: 4,6; 5,6; 3,6; 3.`
+
+int[,] CreateArrayMxN(int rowCount, int colCount, int leftRange = 0, int rightRange = 9)
+{
+    Random rand = new Random();
+    int[,] mass = new int[rowCount, colCount];
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            mass[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return mass;
+}
