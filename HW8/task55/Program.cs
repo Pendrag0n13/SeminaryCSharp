7448ba63f845351dfcf6e2bbd180d50269bfb674
Roman <alvur94@mail.ru>
2023-01-02 20:50:18 +0300

    добавлена функция int[,] CreateArrayMxN(int rowCount, int colCount, int leftRange = 0, int rightRange = 9)

--- HW8/task55/Program.cs
+++ HW8/task55/Program.cs
@@ -1,2 +1,16 @@
 ﻿// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
 // В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+int[,] CreateArrayMxN(int rowCount, int colCount, int leftRange = 0, int rightRange = 9)
+{
+    Random rand = new Random();
+    int[,] mass = new int[rowCount, colCount];
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            mass[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return mass;
+}
