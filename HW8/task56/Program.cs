04297d6afe19d2eeef992b13cc97aebdbf416029
Roman <alvur94@mail.ru>
2023-01-05 16:03:46 +0300

    добавлена функции CreateArrayMxN, GetNumber, PrintMatrix

--- HW8/task56/Program.cs
+++ HW8/task56/Program.cs
@@ -7,3 +7,34 @@
 // 5 2 6 7
 // Программа считает сумму элементов в каждой строке и выдаёт номер строки 
 // с наименьшей суммой элементов: 1 строка
+int[,] CreateArrayMxN(int rowCount, int colCount, int leftRange = 0, int rightRange = 9)
+{
+    Random rand = new Random();
+    int[,] mass = new int[rowCount, colCount];
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            mass[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return mass;
+}
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
