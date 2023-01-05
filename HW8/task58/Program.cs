bfbc85a81b715bbf5e960165e592c6b1c4fb63d8
Roman <alvur94@mail.ru>
2023-01-05 16:45:06 +0300

    добавлена функция int[,] MatrixMultiplication(int[,] A, int[,] B)

--- HW8/task58/Program.cs
+++ HW8/task58/Program.cs
@@ -30,3 +30,22 @@ void PrintMatrix(int[,] matrix)
         Console.WriteLine();
     }
 }
+
+int[,] MatrixMultiplication(int[,] A, int[,] B)
+{
+    int[,] C = new int[A.GetLength(1), B.GetLength(0)];
+    int sum = 0;
+    for (int i = 0; i < C.GetLength(0); i++)
+    {
+        for (int j = 0; j < C.GetLength(1); j++)
+        {
+            sum = 0;
+            for (int k; k < A.GetLength(0); k++)
+            {
+                sum += A[i, k] * B[k, j];
+            }
+            C[i, j] = sum;
+        }
+    }
+    return C;
+}
