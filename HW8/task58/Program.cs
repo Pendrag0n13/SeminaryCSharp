da4e5d4f196d2f707feb5e943e3af2d3d532df36
Roman <alvur94@mail.ru>
2023-01-05 17:07:25 +0300

    добавлена функция void main() и её вызов,  исправлены ошибки

--- HW8/task58/Program.cs
+++ HW8/task58/Program.cs
@@ -25,7 +25,7 @@ void PrintMatrix(int[,] matrix)
     {
         for (int j = 0; j < matrix.GetLength(1); j++)
         {
-            Console.Write(matrix[i, j] + " ");
+            Console.Write(matrix[i, j] + "\t");
         }
         Console.WriteLine();
     }
@@ -39,7 +39,7 @@ void MatrixMultiplication(int[,] A, int[,] B, int[,] C)
         for (int j = 0; j < C.GetLength(1); j++)
         {
             sum = 0;
-            for (int k; k < A.GetLength(0); k++)
+            for (int k = 0; k < A.GetLength(1); k++)
             {
                 sum += A[i, k] * B[k, j];
             }
@@ -50,7 +50,7 @@ void MatrixMultiplication(int[,] A, int[,] B, int[,] C)
 
 bool ValidateMatrixMultiplication(int[,] A, int[,] B)
 {
-    if (A.GetLength(0) == B.GetLength(1))
+    if (A.GetLength(1) == B.GetLength(0))
     {
         return true;
     }
@@ -59,10 +59,44 @@ bool ValidateMatrixMultiplication(int[,] A, int[,] B)
 
 int[,] MatrixProduct(int[,] A, int[,] B)
 {
-    int[,] C = new int[A.GetLength(1), B.GetLength(0)];
+    int[,] C = new int[A.GetLength(0), B.GetLength(1)];
     if (ValidateMatrixMultiplication(A, B))
     {
         MatrixMultiplication(A, B, C);
     }
     return C;
 }
+
+void main()
+{
+    String s = String.Empty;
+    do
+    {
+        Console.Clear();
+        int rowsA = GetNumber("Введите число строк матрицы А: ");
+        int columnsA = GetNumber("Введите число столбцов матрицы А: ");
+        int[,] arrA = CreateArrayMxN(rowsA, columnsA);
+        int rowsB = GetNumber("Введите число строк матрицы B: ");
+        int columnsB = GetNumber("Введите число столбцов матрицы B: ");
+        int[,] arrB = CreateArrayMxN(rowsB, columnsB);
+
+        PrintMatrix(arrA);
+        Console.WriteLine();
+        PrintMatrix(arrB);
+        Console.WriteLine();
+        if(ValidateMatrixMultiplication(arrA, arrB))
+        {
+        PrintMatrix(MatrixProduct(arrA, arrB));
+        }
+        else
+        {
+            Console.WriteLine("Умножение матриц невозможно!");
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("Завершить программу y/n ?");
+        s = Console.ReadLine();
+    } while (s != "y");
+}
+
+main();
