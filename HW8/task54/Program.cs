918487c82431c917ea45cf22783c77c4cd2526e2
Roman <alvur94@mail.ru>
2023-01-05 15:22:09 +0300

    добавлена функции CreateArrayMxN, GetNumber, PrintMatrix

--- HW8/task54/Program.cs
+++ HW8/task54/Program.cs
@@ -8,3 +8,35 @@
 // 1 2 4 7
 // 2 3 5 9
 // 2 4 4 8
+
+int[,] CreateArrayMxN(int rowCount, int colCount, int leftRange = 0, int rightRange = 9)
+{
+    Random rand = new Random();
+    int[,] mass = new int[rowCount, colCount];
+    for (int i = 0; i < mass.GetLength(0); i++)
+    {
+        for (int j = 0; j < mass.GetLength(1); j++)
+        {
+            mass[i, j] = rand.Next(leftRange, rightRange + 1);
+        }
+    }
+    return mass;
+}
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write(matrix[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+}
