92809f8329ec08f564546f751b7fced2fef32dba
Roman <alvur94@mail.ru>
2023-01-08 18:01:25 +0300

    добавлена функция GetNumber

--- HW9/task64/Program.cs
+++ HW9/task64/Program.cs
@@ -2,3 +2,8 @@
 // которая выведет все натуральные числа в промежутке от N до 1.
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
