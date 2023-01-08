80e4cc932838c4e72247a42c914b4e1a32ba2da0
Roman <alvur94@mail.ru>
2023-01-08 17:51:56 +0300

    добавлена функция GetNumber

--- HW9/task69/Program.cs
+++ HW9/task69/Program.cs
@@ -2,3 +2,10 @@
 // и возводит число А в целую степень B с помощью рекурсии.
 // A = 3; B = 5 -> 243 (3⁵)
 // A = 2; B = 3 -> 8
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
+
