ad33bb8686dff991b46fda7223be7e76ba2545ad
Roman <alvur94@mail.ru>
2023-01-08 18:31:01 +0300

    добавлена функция int NatureNumber(int Number, int itr = 1) и вывод результата

--- HW9/task64/Program.cs
+++ HW9/task64/Program.cs
@@ -1,4 +1,4 @@
-﻿// Задача 64: Задайте значение N. Напишите программу, 
+﻿// Задача 64: Задайте значение N. Напишите программу,
 // которая выведет все натуральные числа в промежутке от N до 1.
 // N = 5 -> "5, 4, 3, 2, 1"
 // N = 8 -> "8, 7, 6, 5, 4, 3, 2, 1"
@@ -7,3 +7,17 @@ int GetNumber(string message)
     Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
 }
+
+int NatureNumber(int Number, int itr = 1)
+{
+    if (itr < Number)
+    {
+        Console.Write($"{NatureNumber(Number, itr + 1)}, ");
+    }
+    return itr;
+}
+
+int N = GetNumber("Введите N: ");
+
+Console.Write($"N = {N} —> ");
+Console.WriteLine($"{NatureNumber(N)}");
