c3544ccbf91bb8a30b7eb5b7c0b4b355429a96d9
Roman <alvur94@mail.ru>
2023-01-08 18:50:27 +0300

    добавлена функция int A(int m, int n)

--- HW9/task68/Program.cs
+++ HW9/task68/Program.cs
@@ -1,8 +1,24 @@
-﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии.
 // Даны два неотрицательных числа m и n.
 // m = 2, n = 3 -> A(m,n) = 9
 int GetNumber(string message)
 {
     Console.Write(message);
     return Convert.ToInt32(Console.ReadLine());
+}
+
+int A(int m, int n)
+{
+    if (m == 0)
+    {
+        return n + 1;
+    }
+    else if (m > 0 && n == 0)
+    {
+        return A(m - 1, 1);
+    }
+    else if (m > 0 && n > 0)
+    {
+        return A(m - 1, A(m, n - 1));
+    }
 }
