6deb521371a818806b29b5a9365cb1ecc1aa9f95
Roman <alvur94@mail.ru>
2023-01-08 17:33:21 +0300

    добавлена функция GetNumber

--- HW9/task67/Program.cs
+++ HW9/task67/Program.cs
@@ -2,3 +2,8 @@
 // которая будет принимать на вход число и возвращать сумму его цифр.
 // 453 -> 12
 // 45 -> 9
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
