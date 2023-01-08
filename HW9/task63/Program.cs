2de54e5f75b62a6b09ecf3a5bee063de116ce04d
Roman <alvur94@mail.ru>
2023-01-08 16:46:31 +0300

    добавлена функция GetNumber

--- HW9/task63/Program.cs
+++ HW9/task63/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 63: Задайте значение N. Напишите программу, 
+// которая выведет все натуральные числа в промежутке от 1 до N.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+int GetNumber(string message)
+{
+    Console.Write(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
